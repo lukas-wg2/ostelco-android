2ad5c5cbdfd44a977c9682306a5f1f44ce5ec45b
Martin Cederlof <mape@comoyo.com>
2018-05-09 14:47:51 +0200

    first commit

---
+++ app/src/main/java/org/ostelco/ostelco/MainActivity.kt
@@ -0,0 +1,127 @@
+package org.ostelco.ostelco
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.util.Base64
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.iid.FirebaseInstanceId
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.OnCompleteListener
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    private var mAuth: FirebaseAuth? = null
+    private var token: String = ""
+    private var msisdn: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+
+            mAuth!!.signInWithCustomToken(token)
+                    .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success")
+                            storeFirebaseCloudMessageToken(getToken(), msisdn)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+                        }
+                    })
+        }
+
+        class Auth : AsyncTask<Void, Void, String>() {
+
+            private val TAG = "Auth"
+
+            var client = OkHttpClient()
+
+            override fun doInBackground(vararg params: Void?): String? {
+
+
+                val URL = "http://10.6.4.67:8080/auth/token"
+
+                val builder = Request.Builder()
+                builder.url(URL)
+                val request = builder.build()
+
+                try {
+                    val response = client.newCall(request).execute()
+                    val token = response!!.body().string()
+                    Log.d(TAG, "Response: " + token)
+                    return token
+                } catch (e: Exception) {
+                    Log.e(TAG, "Failed to download", e)
+                }
+                return null
+            }
+
+            public override fun onPostExecute(result: String) {
+                super.onPostExecute(result)
+
+                var claim = result.split(".")[1]
+                val jsonClaim = JSONObject(String(Base64.decode(claim, 0))).getJSONObject("claims")
+                Log.d(TAG, "claim: " + jsonClaim.toString())
+                msisdn = jsonClaim.get("msisdn").toString()
+                Log.d(TAG, "msisdn: " + msisdn)
+
+                val txt = findViewById(R.id.msisdn) as TextView
+                txt.text = msisdn
+
+                token = result
+            }
+        }
+
+        Auth().execute();
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun getToken() : String? {
+        val fbToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Firebase token: " + fbToken)
+        return fbToken
+    }
+
+    fun storeFirebaseCloudMessageToken(token: String?, msisdn: String) {
+        val mDatabase = FirebaseDatabase.getInstance().getReference()
+        mDatabase.child("firebaseCloudMessage").child(msisdn).setValue(token)
+    }
+}
---
+++ app/src/main/java/org/ostelco/ostelco/MyFirebaseInstanceIDService.kt
@@ -0,0 +1,29 @@
+package org.ostelco.ostelco
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
+    private val TAG = "MyFirebaseIDService"
+
+    override fun onTokenRefresh() {
+        // Get updated InstanceID token.
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Refreshed token: " + refreshedToken!!)
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken)
+    }
+
+    /**
+     * Persist token to backend server.
+     * @param token The new token.
+     */
+    private fun sendRegistrationToServer(token: String?) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
